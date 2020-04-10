fbf194da9171c387ce3b856bdad321243de74679
mattkocoloski <matt.kocoloski@gmail.com>
2020-04-10 13:12:43 -0400

    Still trying to figure this out

--- src/sumCpp.cpp
+++ src/sumCpp.cpp
@@ -1,15 +1,18 @@
-// simple function to test whether package was installed and loaded correctly
+// simple test function
 
-#include <Rcpp.h>
+// #include <Rcpp.h>
+#include <RcppArmadillo.h>
 using namespace Rcpp;
 
+// [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
+
 double sumCpp(NumericVector x) {
   
   double value = 0;
   
   for (int i = 0; i < x.length(); ++i) {
-    value = value + x[i];
+    value = value + x(i);
   }
   
   return(value);
